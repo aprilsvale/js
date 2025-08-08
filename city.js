536b7b1773cc158373fea3ea7bbece91b072ee04
Iana <yanaachurinova2001@gmail.com>
2025-08-08 19:52:43 +0500

    great! commit -m

--- city.js
+++ city.js
@@ -3,3 +3,15 @@ const city = "Perm";
 const temperature = 18;
 
 console.log(`Город: ${city}, Температура: ${temperature}°C`);
+
+
+
+const num1 = 7;    
+const num2 = 11;   
+const num3 = 3;   
+
+
+const result = (num1 + num2) * num3;
+
+
+console.log(`Результат: (${num1} + ${num2}) * ${num3} = ${result}`);
