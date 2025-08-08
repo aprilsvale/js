2c76898672a9bfc7e008f9780824f5254c91834b
Iana <yanaachurinova2001@gmail.com>
2025-08-08 20:01:11 +0500

    start

--- city.js
+++ city.js
@@ -15,3 +15,76 @@ const result = (num1 + num2) * num3;
 
 
 console.log(`Результат: (${num1} + ${num2}) * ${num3} = ${result}`);
+
+
+
+let price;
+price = 100;
+price = 150;
+
+console.log(price)
+
+
+
+const text = 'Dinosaur';
+console.log(typeof text);
+const number = 23;
+console.log(typeof number);
+
+
+
+const prod = 2 * 2;
+
+console.log (prod + 2)
+
+
+
+function degreesToRadians(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
+
+const sin54 = Math.sin(degreesToRadians(54));
+const cos16 = Math.cos(degreesToRadians(16));
+
+
+const resultee = Math.pow(sin54 * cos16, 2);
+
+console.log(result); 
+
+
+
+const product = 13.2 * 71.90;
+
+const sqrtProduct = Math.sqrt(product);
+
+const term1 = (16 * sqrtProduct) / 7;
+
+const term2 = Math.pow(3, Math.sqrt(49));
+
+const sum = term1 + term2;
+
+const resulte = sum * Math.pow(2, 7);
+
+console.log(result); 
+
+
+
+
+let a = 3;
+let b = 6;
+let c = 7;
+let d = 9;
+
+console.log (a%2===0);
+
+
+let name;
+name = "Ariana";
+
+
+if (!name) {
+    console.log("Hello, Guest!");
+} else {
+    console.log(`Hello, ${name}!`);
+}
